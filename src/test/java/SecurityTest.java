cf51aca0de40a2511f07bfd865a2497e5df2f461
Sean-M53 <seanmarley40@gmail.com>
2021-12-03 14:19:19 +0000

    Created email tests

--- src/test/java/SecurityTest.java
+++ src/test/java/SecurityTest.java
@@ -2,8 +2,7 @@ import com.example.assignment3.SecurityCheck;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SecurityTest {
     SecurityCheck security;
@@ -12,6 +11,12 @@ public class SecurityTest {
     @DisplayName("Testing emails")
     void securityTest() {
         assertTrue(security.checkEmail("seanmarley40@gmail.com"));
+        assertTrue(security.checkEmail("119454664@umail.ucc.ie"));
+        assertFalse(security.checkEmail("1194@5466@4@umail.ucc.ie"));
+        assertFalse(security.checkEmail("abcdefg"));
+
+
+
     }
 
 }
